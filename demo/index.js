0e7ccc57377831fffaa3739f33007c60f392c189
brickmaker <zhaoxiaodong@zju.edu.cn>
2021-08-06 15:43:20 +0800

    feat: 🎸 barnes-hut approximation

--- demo/index.js
+++ demo/index.js
@@ -7,40 +7,13 @@ const vectorSizeScale = 5;
 const div = d3.select("body");
 const svg = div.append("svg").attr("width", width).attr("height", height);
 
-// See https://developer.mozilla.org/en-US/docs/Web/SVG/Element/marker
-svg
-  .append("defs")
-  .append("marker")
-  .attr("id", "arrow")
-  .attr("viewBox", [0, 0, 8, 4])
-  .attr("refX", 4)
-  .attr("refY", 2)
-  .attr("markerWidth", 8)
-  .attr("markerHeight", 4)
-  .attr("orient", "auto-start-reverse")
-  .append("path")
-  .attr(
-    "d",
-    d3.line()([
-      [0, 0],
-      [0, 4],
-      [8, 2],
-    ])
-  )
-  .attr("fill", "#ff0000");
-
 const nodes = generateRandomData(N, width, height, padding);
 
 // const forces = mockForce(nodes);
-const forces = forceNBody.forceNBodyBruteForce(nodes);
+const forcesBruteForce = forceNBody.forceNBodyBruteForce(nodes);
+const forces = forceNBody.forceNBody(nodes);
+console.log(forcesBruteForce);
 console.log(forces);
-
-const combined = nodes.map((n, i) => ({
-  x: n.x,
-  y: n.y,
-  vx: forces[i].vx,
-  vy: forces[i].vy,
-}));
 
 const node = svg
   .selectAll("circle")
@@ -52,18 +25,53 @@ const node = svg
   .attr("fill", "#67a9ef")
   .attr("r", 5);
 
-const arrow = svg
-  .selectAll("line")
-  .data(combined)
-  .enter()
-  .append("line")
-  .attr("x1", (d) => d.x)
-  .attr("y1", (d) => d.y)
-  .attr("x2", (d) => d.x + vectorSizeScale * d.vx)
-  .attr("y2", (d) => d.y + vectorSizeScale * d.vy)
-  .attr("marker-end", "url(#arrow)")
-  .attr("stroke-width", "2")
-  .attr("stroke", "#ff0000");
+drawArrow(nodes, forcesBruteForce, "#0000ff", "bruteForce");
+drawArrow(nodes, forces, "#ff0000", "force");
+
+function drawArrow(nodes, forces, color, className) {
+  const combined = nodes.map((n, i) => ({
+    x: n.x,
+    y: n.y,
+    vx: forces[i].vx,
+    vy: forces[i].vy,
+  }));
+
+  // See https://developer.mozilla.org/en-US/docs/Web/SVG/Element/marker
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", `arrow-${className}`)
+    .attr("viewBox", [0, 0, 8, 4])
+    .attr("refX", 4)
+    .attr("refY", 2)
+    .attr("markerWidth", 8)
+    .attr("markerHeight", 4)
+    .attr("orient", "auto-start-reverse")
+    .append("path")
+    .attr(
+      "d",
+      d3.line()([
+        [0, 0],
+        [0, 4],
+        [8, 2],
+      ])
+    )
+    .attr("fill", color);
+
+  const arrow = svg
+    .selectAll(`.${className}`)
+    .data(combined)
+    .enter()
+    .append("line")
+    .attr("class", className)
+    .attr("x1", (d) => d.x)
+    .attr("y1", (d) => d.y)
+    .attr("x2", (d) => d.x + vectorSizeScale * d.vx)
+    .attr("y2", (d) => d.y + vectorSizeScale * d.vy)
+    .attr("marker-end", `url(#arrow-${className})`)
+    .attr("stroke-width", "2")
+    .attr("stroke", color);
+}
 
 function generateRandomData(num, width, height, padding) {
   return Array(num)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,101 @@
+import { quadtree } from "d3-quadtree";
 const defaultWeightFunc = () => 30;
 
-export function forceNBody(nodes, weightFunc) {}
+// TODO: 考虑整合，到一个对象里，目前先测试功能
+
+function accumulate(quad) {
+  let accWeight = 0;
+  let accX = 0;
+  let accY = 0;
+
+  if (quad.length) {
+    // internal node, accumulate 4 child quads
+    for (let i = 0; i < 4; i++) {
+      const q = quad[i];
+      if (q && q.weight) {
+        accWeight += q.weight;
+        accX += q.x * q.weight;
+        accY += q.y * q.weight;
+      }
+    }
+    quad.x = accX / accWeight;
+    quad.y = accY / accWeight;
+    quad.weight = accWeight;
+  } else {
+    // leaf node
+    let q = quad;
+    quad.x = q.data.x;
+    quad.y = q.data.y;
+    quad.weight = q.data.weight;
+  }
+}
+
+function computeForce(node, tree) {
+  const theta2 = 0.81;
+  const distanceMin2 = 1;
+  const epsilon = 1e-6;
+
+  const apply = (quad, x1, y1, x2, y2) => {
+    let dx = node.x - quad.x;
+    let dy = node.y - quad.y;
+    const width = x2 - x1;
+    let len2 = Math.max(distanceMin2, dx * dx + dy * dy);
+
+    // far node, apply Barnes-Hut approximation
+    if ((width * width) / theta2 < len2) {
+    // if (false) {
+      // random jiggle
+      if (dx === 0) dx = (Math.random() - 0.5) * epsilon;
+      if (dy === 0) dy = (Math.random() - 0.5) * epsilon;
+
+      node.vx += (dx * quad.weight) / len2;
+      node.vy += (dy * quad.weight) / len2;
+
+      return true;
+    } else {
+      // near quad, compute force directly
+      if (quad.length) return false; // internal node, visit children
+
+      // leaf node
+
+      if (quad.data !== node) {
+        // random jiggle
+        if (dx === 0) dx = (Math.random() - 0.5) * epsilon;
+        if (dy === 0) dy = (Math.random() - 0.5) * epsilon;
+
+        node.vx += (dx * quad.data.weight) / len2;
+        node.vy += (dy * quad.data.weight) / len2;
+      }
+    }
+  };
+
+  tree.visit(apply);
+}
+
+export function forceNBody(nodes, weightFunc = defaultWeightFunc) {
+  const data = nodes.map((n, i) => ({
+    index: i,
+    ...n,
+    vx: 0,
+    vy: 0,
+    weight: weightFunc(n),
+  }));
+
+  const tree = quadtree(
+    data,
+    (d) => d.x,
+    (d) => d.y
+  ).visitAfter(accumulate); // init internal node
+
+  data.forEach((n, i) => {
+    computeForce(n, tree);
+  });
+
+  return data.map((n) => ({
+    vx: n.vx,
+    vy: n.vy,
+  }));
+}
 
 export function forceNBodyBruteForce(nodes, weightFunc = defaultWeightFunc) {
   const distanceMin2 = 1;
